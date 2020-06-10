8a01cf70e349dc8ae34d3841d7a422b0fe7a1f5d
Daniel Karlsson <daniel.combain@gmail.com>
2020-06-10 11:26:48 +0200

    Testing without version check.

--- src/ForegroundPlugin.java
+++ src/ForegroundPlugin.java
@@ -12,10 +12,9 @@ public class ForegroundPlugin extends CordovaPlugin {
     @Override
     @TargetApi(26)
     public boolean execute (final String action, final JSONArray args, final CallbackContext command) throws JSONException {
-        if (android.os.Build.VERSION.SDK_INT >= 26) {
             Activity activity = cordova.getActivity();
             Intent intent = new Intent(activity, ForegroundService.class);
-
+            String success = "FOREGROUND SERVICE: ";
             if (action.equals("start")) {
                 // Tell the service we want to start it
                 intent.setAction("start");
@@ -29,16 +28,17 @@ public class ForegroundPlugin extends CordovaPlugin {
                 
                 // Start the service
                 activity.getApplicationContext().startForegroundService(intent);
+                success += "SHOULD HAVE STARTED";
             } else if (action.equals("stop")) {
                 // Tell the service we want to stop it
                 intent.setAction("stop");
 
                 // Stop the service
                 activity.getApplicationContext().startService(intent);
+                success += "SHOULD HAVE STOPPE";
             }
-        }
 
-        command.success();
+        command.success(success);
         return true;
     }
 }
